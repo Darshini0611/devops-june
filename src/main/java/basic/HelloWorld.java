84cfd64ba25d0f850eb7231fff927e9ea8685c22
Sai Darshini <saidarshini0611@gmail.com>
2025-07-25 21:26:30 +0530

    x1 branch code changes

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -2,7 +2,11 @@ package basic;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		System.out.println("Hello World");//from y1 branch
+=======
+		System.out.println("Hello World");//from x1 branch for stash demo
+>>>>>>> Stashed changes
 	}
 	//from b1 branch
 	public void m1()
